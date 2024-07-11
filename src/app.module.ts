84ad1930bbb9a006b91d8def2b2ccc1189cfb0e2
Morgan <106959238+Mrdotov@users.noreply.github.com>
2024-07-11 17:29:17 +0300

    update auth.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-
-  AuthModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
